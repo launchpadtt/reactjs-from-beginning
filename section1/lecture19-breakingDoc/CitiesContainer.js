caa8b5753c3efdf6ebef3a96473b0d56a2b097f8
Christopher Yee Mon <chrisyeem@protonmail.com>
2026-01-17 00:40:49 -0800

    lecture19

---
+++ section1/lecture19-breakingDoc/CitiesContainer.js
@@ -0,0 +1,17 @@
+class CitiesContainer extends React.Component {
+    render() {
+        const cities = this.props.data.map((city, i) => {
+            const randomImage = `https://picsum.photos/${400 + i}/300`
+            return(
+                <City key={i} city={city} image={randomImage} />
+            )
+        });
+        return(
+            <div className="row">
+                <div className="cities center-align">
+                    {cities}
+                </div>
+            </div>
+        )
+    }
+}
